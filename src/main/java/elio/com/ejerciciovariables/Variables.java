044e967dac2536d6e1c28aa071f1278bd6a853d8
Elio Hoyos <elio18david@gmail.com>
2020-03-19 15:18:53 -0500

    Variable String cadena

---
+++ src/main/java/elio/com/ejerciciovariables/Variables.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elio.com.ejerciciovariables;
+
+/**
+ *
+ * @author FRANKOYCI
+ */
+public class Variables {
+    public static void main(String[] args) {
+        //Variable tipo cadena
+        String saludar = "Hola Elio";
+        System.out.println(saludar);
+        System.out.println(saludar);
+        System.out.println(saludar);
+        
+        var despedida = "Hasta luego";
+        System.out.println(despedida);
+        
+        var numero = 1;
+        System.out.println(numero);
+        System.out.println("Esto es el numero :" + numero);
+        
+        var boleana = true;
+        System.out.println(boleana);
+        
+    }
+}
